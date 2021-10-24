87fe1ef339a8e68487872874564cca74331606b3
williamAstudillo <wsastudilloa@gmail.com>
2021-10-24 18:04:39 -0500

    spealling in _app fixed

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Store from "store/Store";
 import Header from "components/Header/Header";
+import Store from "store/Store";
 import "style.css";
 
 const MyApp = ({ Component, pageProps }) => {
