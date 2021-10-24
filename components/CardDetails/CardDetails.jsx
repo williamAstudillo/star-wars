4cbec4b38f0bfd8d3460341e3180bb8411d17c81
williamAstudillo <wsastudilloa@gmail.com>
2021-10-24 15:10:49 -0500

    fix spelling in component cardDetails

--- components/CardDetails/CardDetails.jsx
+++ components/CardDetails/CardDetails.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Link from "next/Link";
+import Link from "next/link";
 import styles from "components/CardDetails/cardDetails.module.css";
 
 const CardDetails = ({ character, setShowModal, setName, index }) => {
